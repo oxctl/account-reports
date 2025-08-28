a3b44c895057b1aed87e533c0c5ea88cdf12068e
lardo_de_arnaud <adamm@ox.ac.uk>
2025-08-28 14:25:19 +0100

    added javadoc

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,9 @@
+/**
+ * Capitalizes the first letter of a given string.
+ *
+ * @param {string} val - The input string to be processed.
+ * @returns {string} - The input string with the first letter capitalized.
+ */
 export function capitalizeFirstLetter(val) {
 	if (!val) return "";
   return val.charAt(0).toUpperCase() + val.slice(1);
