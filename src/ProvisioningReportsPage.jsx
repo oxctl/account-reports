b3e00692b9663fc2cd2762bf7dd2f8c1c819f022
lardo-de-arnaud <lardo-de-arnaud@users.noreply.github.com>
2025-09-25 14:29:01 +0000

    style: format files

--- src/ProvisioningReportsPage.jsx
+++ src/ProvisioningReportsPage.jsx
@@ -100,13 +100,15 @@ function ProvisioningReportsPage({ token, server, accountId, handle40x }) {
             attachment: { url = "" } = {},
           } = report;
 
-          let mainTitle = ""
+          let mainTitle = "";
           if (extra_text) {
-          // Extract main title (e.g., "Users", "Courses")
-             mainTitle = capitalizeFirstLetter(extra_text?.match(/Reports.*$/)?.[0]?.replace("Reports: ", "") ?? "");
-          }
-          else {
-	         mainTitle = "Report with ID '"+id+"' not yet completed.";
+            // Extract main title (e.g., "Users", "Courses")
+            mainTitle = capitalizeFirstLetter(
+              extra_text?.match(/Reports.*$/)?.[0]?.replace("Reports: ", "") ??
+                "",
+            );
+          } else {
+            mainTitle = "Report with ID '" + id + "' not yet completed.";
           }
 
           // Extract optional extra info (like term)
