ab2dedfa2eb49606f044e0c6e7cd46ccb19627fa
lardo-de-arnaud <lardo-de-arnaud@users.noreply.github.com>
2025-09-26 16:15:00 +0000

    style: format files

--- deployment/deployment-tests.test.js
+++ deployment/deployment-tests.test.js
@@ -1,32 +1,43 @@
-import { test, expect } from '@playwright/test'
-import { goToTool, addNetworkThrottle, NETWORK_PRESETS, screenshot, getToolAnchor } from './test-utils'
-import { networkPreset } from './constants'
+import { test, expect } from "@playwright/test";
+import {
+  goToTool,
+  addNetworkThrottle,
+  NETWORK_PRESETS,
+  screenshot,
+  getToolAnchor,
+} from "./test-utils";
+import { networkPreset } from "./constants";
 
-test('Account tools deployment tests at subaccount', async ({context, page}, testInfo) => {
+test("Account tools deployment tests at subaccount", async ({
+  context,
+  page,
+}, testInfo) => {
+  let ltiToolFrame;
 
-  let ltiToolFrame
-
-  await test.step('Load the tool', async () => {
+  await test.step("Load the tool", async () => {
     if (networkPreset) {
-      await addNetworkThrottle(context, page, NETWORK_PRESETS[networkPreset])
+      await addNetworkThrottle(context, page, NETWORK_PRESETS[networkPreset]);
     }
-    ltiToolFrame = await goToTool(page)
-    if (page.url().includes('beta')) {
+    ltiToolFrame = await goToTool(page);
+    if (page.url().includes("beta")) {
       // the warning banner on beta interferes with some click operations so dismiss it
-      await page.getByRole('button', {name: 'Close warning'}).click()
+      await page.getByRole("button", { name: "Close warning" }).click();
     }
-  })
+  });
 
-  await test.step('Tool loads', async () => {
+  await test.step("Tool loads", async () => {
     const title = await getToolAnchor(ltiToolFrame);
-    await expect(title).toBeVisible()
-  })
+    await expect(title).toBeVisible();
+  });
 
-  await test.step('Report(s) are available to run', async () => {
-    const buttons = await ltiToolFrame.getByRole('button')
-    await expect(buttons).toHaveCount(1)
+  await test.step("Report(s) are available to run", async () => {
+    const buttons = await ltiToolFrame.getByRole("button");
+    await expect(buttons).toHaveCount(1);
 
-    const multipleLoginUsers = await ltiToolFrame.getByText('Multiple Login Users', {exact: true})
-    await expect(multipleLoginUsers).toBeVisible()
-  })
-})
+    const multipleLoginUsers = await ltiToolFrame.getByText(
+      "Multiple Login Users",
+      { exact: true },
+    );
+    await expect(multipleLoginUsers).toBeVisible();
+  });
+});
--- deployment/playwright.config.js
+++ deployment/playwright.config.js
@@ -1,36 +1,36 @@
 const config = {
   projects: [
     {
-      name: 'assertVariables',
-      testMatch: /assertVariables.js/
+      name: "assertVariables",
+      testMatch: /assertVariables.js/,
     },
     // Setup project
     {
-      name: 'setup',
+      name: "setup",
       testMatch: /.*\.setup\.js/,
-      dependencies: ['assertVariables']
+      dependencies: ["assertVariables"],
     },
     {
-      name: 'chromium',
+      name: "chromium",
       use: {
         // Use prepared auth state.
-        storageState: 'user.json',
+        storageState: "user.json",
       },
-      dependencies: [ 'assertVariables', 'setup' ]
-    }
+      dependencies: ["assertVariables", "setup"],
+    },
   ],
   timeout: 60000,
-  reporter: [ ['html', { open: 'never' }] ],
+  reporter: [["html", { open: "never" }]],
   retries: 2,
   use: {
     // useful for debugging locally
     // headless: false,
     // viewport: { width: 1280, height: 720 },
     // ignoreHTTPSErrors: true,
-    video: 'on',
+    video: "on",
     launchOptions: {
-      slowMo: 500
-    }
+      slowMo: 500,
+    },
   },
 };
 
