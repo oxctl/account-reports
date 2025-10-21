d91ae07b37d59dfc43067c81612d1c6d35b87c1b
lardo_de_arnaud <adamm@ox.ac.uk>
2025-10-21 17:51:18 +0100

    Revert "AB#94267 UX: stabilize scroll after pagination via top anchor focus and hard scroll"
    This reverts commit c727967b7b8104612cf93e49b4af8b0fdb1a481e.

--- src/DateFilterPage.jsx
+++ src/DateFilterPage.jsx
@@ -110,6 +110,7 @@ function DateFilterPage({ token, server, accountId, handle40x }) {
   }, [token, currentPageUrl]);
 
   // Scroll-to-top behavior removed
+  // Scroll-to-top behavior removed
 
   // Validate that the before/after pair is chronological. Only validate when
   // both values are provided; if one or both are blank we allow the filter to
