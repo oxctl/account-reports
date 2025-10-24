1bf45260afba55eda5fd21d177371f17088df71c
lardo_de_arnaud <adamm@ox.ac.uk>
2025-10-24 15:46:24 +0100

    Revert "AB#94267 UX: scroll to top on pagination next click"
    This reverts commit 2834132fcdb99ca6c5764035d1219a1e638b0201.

--- src/AddPagination.jsx
+++ src/AddPagination.jsx
@@ -5,16 +5,6 @@ export function AddPagination({ prevUrl, currUrl, nextUrl, setCurrUrl }) {
     setCurrUrl(url);
   };
 
-  const scrollToTop = () => {
-    try {
-      if (typeof window !== "undefined" && window.scrollTo) {
-        window.scrollTo({ top: 0, behavior: "smooth" });
-      }
-    } catch (e) {
-      // no-op if scrolling fails for any reason
-    }
-  };
-
   // Extract the current page number from the current URL (not per_page)
   const pageNumber = (() => {
     try {
@@ -41,14 +31,7 @@ export function AddPagination({ prevUrl, currUrl, nextUrl, setCurrUrl }) {
       )}
       <Pagination.Page>{pageNumber}</Pagination.Page>
       {nextUrl && (
-        <Pagination.Page
-          onClick={() => {
-            onClick(nextUrl);
-            scrollToTop();
-          }}
-        >
-          &gt;
-        </Pagination.Page>
+        <Pagination.Page onClick={() => onClick(nextUrl)}>&gt;</Pagination.Page>
       )}
     </Pagination>
   );
