ebcd5f3835db694c4af6122eabac369f5f5fa55d
lardo_de_arnaud <adamm@ox.ac.uk>
2025-10-24 11:25:11 +0100

    AB#95177: Remove background prop from ToggleDetails in DateFilterPageToggleGroup

--- src/DateFilterPageToggleGroup.jsx
+++ src/DateFilterPageToggleGroup.jsx
@@ -4,8 +4,6 @@ function DateFilterPageToggleGroup({ expanded, setExpanded, children }) {
   return (
     <ToggleDetails
       summary="Apply Filters"
-      background="transparent"
-      transition={true}
       themeOverride={{ borderColor: "transparent", borderWidth: "0" }}
       expanded={expanded}
       onToggle={() => setExpanded(!expanded)}
