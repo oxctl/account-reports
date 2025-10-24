43178a9bfed76313a3b67870374528dfac3f1f03
lardo_de_arnaud <adamm@ox.ac.uk>
2025-10-24 10:15:50 +0100

    AB#95177: remove unsupported toggleLabel prop and use boolean transition on ToggleDetails

--- src/DateFilterPageToggleGroup.jsx
+++ src/DateFilterPageToggleGroup.jsx
@@ -3,10 +3,9 @@ import { ToggleDetails } from "@instructure/ui-toggle-details";
 function DateFilterPageToggleGroup({ expanded, setExpanded, children }) {
   return (
     <ToggleDetails
-      toggleLabel="Show or hide filter controls"
       summary="Apply Filters"
       background="transparent"
-      transition="true"
+      transition={true}
       themeOverride={{ borderColor: "transparent", borderWidth: "0" }}
       expanded={expanded}
       onToggle={() => setExpanded(!expanded)}
