96e501f791bff99b2669c60a34d91d4bab79a614
lardo_de_arnaud <adamm@ox.ac.uk>
2025-11-04 14:56:36 +0000

    AB#94135: commit outstanding changes

--- deployment/deployment.test.js
+++ deployment/deployment.test.js
@@ -10,7 +10,10 @@ test.describe('Test deployment', async () => {
     await dismissBetaBanner(page)
     const ltiIFrame = getLtiIFrame(page)
     await waitForNoSpinners(ltiIFrame)
-    
+
+    // shouldnt need to wait but oxeval was acting strangely! (Waiting didnt cure the problem.)
+    // await new Promise(r => setTimeout(r, 10000)); 
+
     // Check there's a title Account Reports
     const title = ltiIFrame.getByText("Account Reports")
     await expect(title).toBeVisible();
