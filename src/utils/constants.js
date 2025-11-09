a6ec6fa4937db2f223f7c16c4a1ff687a85bf2b3
lardo_de_arnaud <adamm@ox.ac.uk>
2025-11-09 12:39:24 +0000

    AB#106873 chore: save workspace changes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -7,12 +7,6 @@ export const ROOT_ACCOUNT_ID = JSON.parse(
   import.meta.env.VITE_APP_ROOT_ACCOUNT_ID,
 );
 
-// Enrollment report config (support both VITE_ and VITE_APP_ prefixes)
-export const ENROL_REPORT_NAME_1 =
-  import.meta.env.VITE_ENROL_REPORT_NAME_1 || import.meta.env.VITE_APP_ENROL_REPORT_NAME_1 || import.meta.env.ENROL_REPORT_NAME_1 || "";
-
-export const ENROL_REPORT_ID_1 =
-  (import.meta.env.VITE_ENROL_REPORT_ID_1 || import.meta.env.VITE_APP_ENROL_REPORT_ID_1 || import.meta.env.ENROL_REPORT_ID_1 || "");
 
 // Number of enrolment reports (support VITE_ and VITE_APP_ prefixes)
 export const NUMBER_ENROL_REPORTS = Number(
