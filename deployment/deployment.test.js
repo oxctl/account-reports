00590861db896788b8fe03e69ab41f94bb77d7aa
Adam Marshall <adam.marshall@it.ox.ac.uk>
2025-11-14 18:44:48 +0000

    removed blank line so as to re-run deployment tests

--- deployment/deployment.test.js
+++ deployment/deployment.test.js
@@ -2,7 +2,6 @@ import { test, expect } from '@playwright/test'
 import { dismissBetaBanner, getLtiIFrame, waitForNoSpinners, TEST_URL } from '@oxctl/deployment-test-utils'
 
 // TEST_URL is provided by @oxctl/deployment-test-utils and composes host+path
-
 test.describe('Test deployment', () => {
     test('The tool should load and the title "Account Reports" should be shown', async ({context, page}) => {
     await page.goto(TEST_URL)
